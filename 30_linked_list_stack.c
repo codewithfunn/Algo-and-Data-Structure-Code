b91a5179153402e0c658d3368ca93b01ed93e0af
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-01-21 12:25:40 +0530

    linkedList stack push,pop,isEmpty,isFull operation

---
+++ 30_linked_list_stack.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node * next;
+};
+// traversal
+void traversal(struct Node *ptr){
+    while(ptr!=NULL){
+        printf("Element is :: %d\n",ptr->data);
+        ptr = ptr->next;
+    }
+}
+// isEmpty
+int isEmpty(struct Node *top){
+    if(top == NULL){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+// isfull
+int isFull(struct Node *top){
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    if(p==NULL){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+ 
+//push operation
+struct Node * push(struct Node *top, int data){
+    if(isFull(top)){
+        printf("The strack is Overflow! Connot insert %d in the strack\n",data);
+        // return -1;
+    }
+    else{
+        struct Node * n = (struct Node *)malloc(sizeof(struct Node));
+        n->data = data;        
+        n->next = top;
+        top = n;
+        return top;
+    }
+}
+// pop operation
+int  pop(struct Node **top){
+    if(isEmpty(*top)){
+        printf("The stack is Underflow! Cannot perform pop operation on the stack");
+        return -1;
+    }else{
+        struct Node *n = *top;
+        *top = (*top)->next; // de-refernce nd next 
+        int data = n->data;
+        free(n);
+        return data;
+    }
+}
+int main(){
+    // |top|->| data | ptr |-->| data | ptr |-->|data | ptr |-->NULL
+    struct Node *top=NULL;// intialize the top with Null
+   
+    top = push(top , 0);
+    top = push(top , 1);
+    top = push(top , 2);
+    top = push(top , 3);
+    printf("popped element is %d\n",pop(&top));
+    traversal(top);
+    return 0 ;
+}
