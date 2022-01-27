02151d9499d3c9deed1a5e6e4feaa764f776c783
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-01-27 19:27:18 +0530

    preOrder traversal of binary tree

---
+++ 67_preOrder_traversal_in_Binary_tree.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node * createNode(int data){
+    struct node *n;
+    n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+void preOrder_traversal(struct node *root){
+    if(root!=NULL){
+        printf(" %d ",root->data);
+        preOrder_traversal(root->left);
+        preOrder_traversal(root->right);
+    }
+}
+int main(){
+    // Constructing the node - Using function (Recommended)
+    struct node *p = createNode(4);
+    struct node *p1 = createNode(1);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(5);
+    struct node *p4 = createNode(2);
+
+    // Finally the tree looks like this:
+    //      4
+    //     / \
+    //    1   6
+    //   / \
+    //  5   2
+
+    // linking the root node with left and right children
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    preOrder_traversal(p);
+    return 0 ;
+}
